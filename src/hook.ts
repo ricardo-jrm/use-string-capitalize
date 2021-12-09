cd7e60055ea740bc2150c8b069ee2127dc6f7d75
l1b3r_- <liber@ricardojrm.com>
2021-12-09 06:39:03 +0000

    chore: declare return type

--- src/hook.ts
+++ src/hook.ts
@@ -4,5 +4,5 @@ import { stringCapitalize } from '@ricardo-jrm/string-capitalize';
 /**
  * Capitalizes the first character of a given string. Option to capitalize all words.
  */
-export const useStringCapitalize = (str: string, allWords?: boolean) =>
+export const useStringCapitalize = (str: string, allWords?: boolean): string =>
   useMemo(() => stringCapitalize(str, allWords), [str, allWords]);
