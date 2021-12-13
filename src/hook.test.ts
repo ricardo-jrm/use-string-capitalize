fc25b769fb37e150532bf403d4acfd40fe87cae0
l1b3r_- <liber@ricardojrm.com>
2021-12-13 03:27:04 +0000

    test: add hook tests

---
+++ src/hook.test.ts
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useStringCapitalize } from '.';
+
+describe('test example hook', () => {
+  it('should capitalize a string', () => {
+    const { result } = renderHook(() =>
+      useStringCapitalize('lorem ipsum dolor sit amet'),
+    );
+    expect(result.current).toBe('Lorem ipsum dolor sit amet');
+  });
+  it('should capitalize all words in a string', () => {
+    const { result } = renderHook(() =>
+      useStringCapitalize('lorem ipsum dolor sit amet', true),
+    );
+    expect(result.current).toBe('Lorem Ipsum Dolor Sit Amet');
+  });
+});
